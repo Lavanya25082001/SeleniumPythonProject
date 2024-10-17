cba5e804f3cf2120bcc71996ccf41a381d35028f
Lavanya25082001 <137297929+Lavanya25082001@users.noreply.github.com>
2024-10-17 18:10:52 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 call C:\Users\Lavanya.Gorrela\PycharmProjects\SeleniumPythonProject\.venv\scripts\activate
-rem pytest -v -s .\test_cases\test_admin_login.py
- pytest -v -s .\test_cases\test_categories.py
+C:\Users\Lavanya.Gorrela\PycharmProjects\SeleniumPythonProject\.venv\Scripts\python -m pytest -v -s .\test_cases\test_admin_login.py
+C:\Users\Lavanya.Gorrela\PycharmProjects\SeleniumPythonProject\.venv\Scripts\python -m pytest -v -s .\test_cases\test_categories.py
 pause
